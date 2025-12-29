733cf8c6b4165b897ead57920f0b0351e1527c7f
Nicole Alcala <nicolealcalq@gmail.com>
2025-12-29 15:02:04 +0800

    Add EmptyApplications UI

---
+++ client/src/components/applications/EmptyApplications.tsx
@@ -0,0 +1,28 @@
+import Button from "@mui/material/Button";
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import NoDocumentIllustration from "../svg/NoDocumentIllustration";
+import { Box } from "@mui/material";
+
+function EmptyApplications() {
+  return (
+    <Box className="flex flex-col justify-center items-center-safe rounded-xl bg-white h-full w-full border-3 border-dashed">
+      <NoDocumentIllustration />
+      <p>
+        <strong>No applications yet</strong>
+      </p>
+      <p className="opacity-70">Insert an application record to get started</p>
+      <Button
+        variant="contained"
+        size="large"
+        sx={{
+          marginTop: "32px",
+        }}
+        startIcon={<AddRoundedIcon />}
+      >
+        Add application
+      </Button>
+    </Box>
+  );
+}
+
+export default EmptyApplications;
