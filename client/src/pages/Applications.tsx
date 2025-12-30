c3ea038bf657a32d5a0ac2c0b8b36ba52afccf27
Nicole Alcala <nicolealcalq@gmail.com>
2025-12-30 17:56:49 +0800

    Add conditional rendering for empty applications state

--- client/src/pages/Applications.tsx
+++ client/src/pages/Applications.tsx
@@ -1,11 +1,12 @@
 import ApplicationsTable from "../components/applications/ApplicationsTable";
+import EmptyApplications from "../components/applications/EmptyApplications";
+import { mockData } from "../lib/table/applications";
 //import EmptyApplications from "../components/applications/EmptyApplications";
 
 export default function ApplicationsPage() {
   return (
     <article className="size-full">
-      {/* <EmptyApplications /> */}
-      <ApplicationsTable />
+      {mockData.length > 0 ? <ApplicationsTable /> : <EmptyApplications />}
     </article>
   );
 }
