5a3c648aa1acc9003a52ed905b68ddd0b0d204ee
Nicole Alcala <nicolealcalq@gmail.com>
2025-12-30 17:58:05 +0800

    Update theme for components

--- client/src/lib/config/theme.ts
+++ client/src/lib/config/theme.ts
@@ -4,13 +4,35 @@ const theme = createTheme({
   typography: {
     fontFamily: "'Figtree', sans-serif",
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+          boxShadow: "none",
+          fontWeight: 600,
+        },
+        sizeLarge: {
+          height: "40px",
+        },
+      },
+    },
+    MuiListItemIcon: {
+      styleOverrides: {
+        root: {
+          minWidth: "0 !important",
+          marginRight: "12px",
+        },
+      },
+    },
+  },
   palette: {
     mode: "light",
     primary: {
       main: "#6708fa",
       light: "#7c86ff",
       dark: "#432dd7",
-      contrastText: "rgba(255,255,255,0.87)",
+      contrastText: "rgba(255,255,255,0.95)",
     },
     secondary: {
       main: "#ff4001",
@@ -21,13 +43,13 @@ const theme = createTheme({
       main: "#00c951",
       light: "#05df72",
       dark: "#008236",
-      contrastText: "rgba(255,255,255,0.87)",
+      contrastText: "rgba(255,255,255,0.95)",
     },
     error: {
       main: "#fb2c36",
       light: "#ff6467",
       dark: "#c10007",
-      contrastText: "rgba(255,255,255,0.87)",
+      contrastText: "rgba(255,255,255,0.95)",
     },
     warning: {
       main: "#fd9a00",
