0ca5940b2f31a0ed383a195eb8107f71f9d9ec13
Nicole Alcala <nicolealcalq@gmail.com>
2026-01-02 13:38:03 +0800

    Define "src" path alias

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +18,9 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
