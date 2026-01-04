617f94736c9164f6b3b46ca95e9d4cd7d27ce426
Nicole Alcala <nicolealcalq@gmail.com>
2026-01-04 18:21:47 +0800

    Initial Auth Page

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,8 +1,8 @@
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
-export default function Auth() {
+export default function AuthPage() {
   <Box component="article" className="size-full">
-    <Typography>Auth</Typography>
+    <Typography>Auth Page</Typography>
   </Box>;
 }
