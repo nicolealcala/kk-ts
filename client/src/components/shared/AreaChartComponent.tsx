faeebbc6d75ba83ab02fd8aea9ce2265fe25b498
Nicole Alcala <nicolealcalq@gmail.com>
2026-01-04 14:14:16 +0800

    Update component to cater dynamic colos

--- client/src/components/shared/AreaChartComponent.tsx
+++ client/src/components/shared/AreaChartComponent.tsx
@@ -1,18 +1,24 @@
 import { LineChart } from "@mui/x-charts/LineChart";
 
-function GradientFill() {
-  // Define gradient stops (adjust colors and offsets as needed)
-  // Using `yScale` helps position colors relative to data values
-  const gradientStops = [
-    { offset: 0, stopColor: "#7c86ff" }, // Top color
-    { offset: 100, stopColor: "rgba(131, 166, 255, 0)" }, // Bottom (transparent)
-  ];
+export type GradientStops = {
+  offset: number;
+  stopColor: string;
+};
 
+const gradients = [
+  { offset: 0, stopColor: "#7c86ff" }, // Top color
+  { offset: 100, stopColor: "rgba(131, 166, 255, 0)" }, // Bottom (transparent)
+];
+function GradientFill({
+  gradientStops = gradients,
+}: {
+  gradientStops?: GradientStops[];
+}) {
   return (
     <svg width="0" height="0">
       <defs>
         <linearGradient
-          id="areaGradient"
+          id={`areaGradient-${gradientStops[0].stopColor}`}
           x1="0"
           y1="0"
           x2="0"
@@ -31,7 +37,13 @@ function GradientFill() {
   );
 }
 
-export default function AreaChartComponent() {
+export default function AreaChartComponent({
+  areaGradients,
+  color,
+}: {
+  areaGradients: GradientStops[];
+  color: string;
+}) {
   return (
     <LineChart
       margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
@@ -47,19 +59,18 @@ export default function AreaChartComponent() {
         {
           data: [2, 5.5, 2, 8.5, 1.5, 5],
           area: true,
-          color: "#6708fa",
-
+          color: color || "#6708fa",
           showMark: false,
         },
       ]}
       sx={{
         p: 0,
         "& .MuiAreaElement-root": {
-          fill: "url(#areaGradient)",
+          fill: `url(#areaGradient-${areaGradients[0].stopColor})`,
         },
       }}
     >
-      <GradientFill />
+      <GradientFill gradientStops={areaGradients} />
     </LineChart>
   );
 }
