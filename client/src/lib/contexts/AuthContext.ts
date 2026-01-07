3b5430fdee0db150aa04343ee664d232b5084335
Nicole Alcala <nicolealcalq@gmail.com>
2026-01-07 15:51:35 +0800

    Update context an add separate component for context provider

--- client/src/lib/contexts/AuthContext.ts
+++ client/src/lib/contexts/AuthContext.ts
@@ -1,10 +1,14 @@
 import { createContext, useContext } from "react";
-
+import { AuthError, type Session } from "@supabase/supabase-js";
 export type AuthMode = "login" | "signup" | null;
 
 export interface Auth {
   authMode: AuthMode;
   setAuthMode: React.Dispatch<React.SetStateAction<AuthMode>>;
+  authError: AuthError | null;
+  setAuthError: React.Dispatch<React.SetStateAction<AuthError | null>>;
+  authSession: Session | null;
+  setAuthSession: React.Dispatch<React.SetStateAction<Session | null>>;
 }
 
 export const AuthContext = createContext<Auth | undefined>(undefined);
