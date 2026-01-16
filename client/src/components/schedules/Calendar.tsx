62441296ae15c76b31f553ace4875302ea018069
Nicole Alcala <nicolealcalq@gmail.com>
2026-01-16 15:34:58 +0800

    Refactor: Update Schedule Calendar, add custom toolbar with MUI components

--- client/src/components/schedules/Calendar.tsx
+++ client/src/components/schedules/Calendar.tsx
@@ -8,17 +8,21 @@ import {
 import { DateTime } from "luxon";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "@/styles/schedules.scss";
-import { useState, type SetStateAction } from "react";
+import { useMemo, useState, type SetStateAction } from "react";
 import { schedules } from "@/lib/mock-data/schedules";
 import React from "react";
+import CustomToolbar, {
+  type CalendarEvent,
+  type CustomToolbarProps,
+} from "./CalendarToolbar";
 
 const localizer = luxonLocalizer(DateTime);
 
-const events = schedules.map((s) => ({
+const events: CalendarEvent[] = schedules.map((s) => ({
   ...s,
   start: DateTime.fromISO(s.start).toJSDate(),
   end: DateTime.fromISO(s.end).toJSDate(),
-}));
+})) as CalendarEvent[];
 
 type ScheduleCalendar = {
   setOpenDrawer: React.Dispatch<SetStateAction<boolean>>;
@@ -27,34 +31,48 @@ type ScheduleCalendar = {
 
 export default function ScheduleCalendar({
   setOpenDrawer,
-  setSelectedSlot,
-}: ScheduleCalendar) {
+}: //setSelectedSlot,
+ScheduleCalendar) {
   const [view, setView] = useState<View>(Views.WEEK);
   const [date, setDate] = useState(new Date());
 
-  const handleSelectSlot = (slotProps: SlotInfo) => {
-    console.log(slotProps);
-    setOpenDrawer(true);
-    setSelectedSlot(slotProps);
-  };
+  // const handleSelectSlot = (slotProps: SlotInfo) => {
+  //   console.log(slotProps);
+  //   setOpenDrawer(true);
+  //   setSelectedSlot(slotProps);
+  // };
+
+  const { components } = useMemo(
+    () => ({
+      components: {
+        toolbar: (props: CustomToolbarProps) => (
+          <CustomToolbar {...props} onAdd={() => setOpenDrawer(true)} />
+        ),
+      },
+    }),
+    [setOpenDrawer]
+  );
 
   return (
-    <Calendar
-      selectable={true}
+    <Calendar<CalendarEvent>
+      //selectable
+      //onSelectSlot={handleSelectSlot}
       localizer={localizer}
       events={events}
+      //For calendar display options
       views={[Views.MONTH, Views.WEEK, Views.DAY]}
       defaultView={view}
-      view={view} // Include the view prop
-      date={date} // Include the date prop
+      view={view}
       onView={(view) => setView(view)}
+      // For date range navigations
+      date={date}
       onNavigate={(date) => {
         setDate(new Date(date));
       }}
       onSelectEvent={(e) => console.log(e)}
-      onSelectSlot={handleSelectSlot}
-      startAccessor="start"
-      endAccessor="end"
+      // startAccessor="start"
+      // endAccessor="end"
+      components={components}
     />
   );
 }
