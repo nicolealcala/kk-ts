4141ff807a6855083726c97bbf65d0e48f1a12e8
Nicole Alcala <nicolealcalq@gmail.com>
2026-01-29 22:16:45 +0800

    Update default values

--- client/src/mocks/data/applicationsData.ts
+++ client/src/mocks/data/applicationsData.ts
@@ -1,82 +1,5 @@
 import type { Application } from "@/lib/types/applications"; // Assuming types are in a separate file
 
-// const applicationsData: Application[] = [
-//   {
-//     id: "1",
-//     organization: "Company A",
-//     position: "Software Engineer",
-//     location: {
-//       city: "Makati",
-//       country: "Philippines",
-//     },
-//     workArrangement: "remote",
-//     source: { platform: "LinkedIn", link: "#" },
-//     statusHistory: [{ status: "applied", date: "2025-05-10T08:00:00Z" }],
-//     salary: {
-//       currency: "PHP",
-//       minAmount: 80000,
-//       maxAmount: 120000,
-//     },
-//     createDate: "2025-05-10T08:00:00Z",
-//     updateDate: "2025-05-10T08:00:00Z",
-//   },
-//   {
-//     id: "2",
-//     organization: "Company B",
-//     position: "Data Scientist",
-//     location: {
-//       city: "Makati",
-//       country: "Philippines",
-//     },
-//     workArrangement: "remote",
-//     source: { platform: "Indeed", link: "#" },
-//     statusHistory: [
-//       { status: "applied", date: "2024-04-20T10:30:00Z" },
-//       { status: "interviewing", date: "2024-04-22T14:00:00Z" },
-//     ],
-//     salary: {
-//       currency: "USD",
-//       minAmount: 4000,
-//       maxAmount: 6000,
-//     },
-//     createDate: "2024-04-20T10:30:00Z",
-//     updateDate: "2024-04-22T14:00:00Z",
-//   },
-//   {
-//     id: "3",
-//     organization: "Company C",
-//     position: "Product Manager",
-//     location: {
-//       city: "Makati",
-//       country: "Philippines",
-//     },
-//     workArrangement: "hybrid",
-//     source: { platform: "JobStreet", link: "#" },
-//     statusHistory: [{ status: "offered", date: "2024-03-15T09:15:00Z" }],
-//     salary: {
-//       currency: "PHP",
-//       maxAmount: 150000,
-//     },
-//     createDate: "2024-03-01T11:00:00Z",
-//     updateDate: "2024-03-15T09:15:00Z",
-//   },
-//   {
-//     id: "4",
-//     organization: "Company D",
-//     position: "UX Designer",
-//     location: {
-//       city: "Makati",
-//       country: "Philippines",
-//     },
-//     workArrangement: "remote",
-//     source: { platform: "JobStreet", link: "#" },
-//     statusHistory: [{ status: "rejected", date: "2024-02-28T16:45:00Z" }],
-//     // Testing optionality: no salary provided for this entry
-//     createDate: "2024-02-15T13:20:00Z",
-//     updateDate: "2024-02-28T16:45:00Z",
-//   },
-// ];
-
 export const applicationsData: Application[] = [
   {
     id: "1",
@@ -89,13 +12,14 @@ export const applicationsData: Application[] = [
       country: "Philippines",
     },
     workArrangement: "remote",
-    source: { platform: "LinkedIn", link: "#" },
+    source: { platform: "linkedin", link: "#" },
     statusHistory: [{ status: "applied", date: "2025-05-10T08:00:00Z" }],
     salary: {
       currency: "PHP",
       minAmount: 80000,
       maxAmount: 120000,
     },
+    type: "fulltime",
     createDate: "2025-05-10T08:00:00Z",
     updateDate: "2025-05-10T08:00:00Z",
   },
@@ -106,11 +30,10 @@ export const applicationsData: Application[] = [
     description:
       "Building predictive models for customer churn using Python and SQL.",
     location: {
-      city: "Makati",
-      country: "Philippines",
+      country: "USA",
     },
     workArrangement: "remote",
-    source: { platform: "Indeed", link: "#" },
+    source: { platform: "indeed", link: "#" },
     statusHistory: [
       { status: "applied", date: "2024-04-20T10:30:00Z" },
       { status: "interviewing", date: "2024-04-22T14:00:00Z" },
@@ -120,6 +43,7 @@ export const applicationsData: Application[] = [
       minAmount: 4000,
       maxAmount: 6000,
     },
+    type: "contract",
     createDate: "2024-04-20T10:30:00Z",
     updateDate: "2024-04-22T14:00:00Z",
   },
@@ -134,12 +58,13 @@ export const applicationsData: Application[] = [
       country: "Philippines",
     },
     workArrangement: "hybrid",
-    source: { platform: "JobStreet", link: "#" },
+    source: { platform: "jobstreet", link: "#" },
     statusHistory: [{ status: "offered", date: "2024-03-15T09:15:00Z" }],
     salary: {
       currency: "PHP",
       maxAmount: 150000,
     },
+    type: "fulltime",
     createDate: "2024-03-01T11:00:00Z",
     updateDate: "2024-03-15T09:15:00Z",
   },
@@ -153,8 +78,9 @@ export const applicationsData: Application[] = [
       country: "Philippines",
     },
     workArrangement: "remote",
-    source: { platform: "JobStreet", link: "#" },
-    statusHistory: [{ status: "rejected", date: "2024-02-28T16:45:00Z" }],
+    source: { platform: "jobstreet", link: "#" },
+    statusHistory: [{ status: "not-selected", date: "2024-02-28T16:45:00Z" }],
+    type: "gig",
     createDate: "2024-02-15T13:20:00Z",
     updateDate: "2024-02-28T16:45:00Z",
   },
