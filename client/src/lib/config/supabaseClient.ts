3fe2d1998caa86e0f5e7bdbcec18e852deb64dcd
Nicole Alcala <nicolealcalq@gmail.com>
2026-07-17 16:58:04 +0800

    Updated Supabase Client

--- client/src/lib/config/supabaseClient.ts
+++ client/src/lib/config/supabaseClient.ts
@@ -4,7 +4,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
