71a7a5d07106758558e9a51d21225b09f55e1cc4
Nicole Alcala <nicolealcalq@gmail.com>
2026-08-13 22:49:38 +0800

    Updated users mock seed properties

--- server/src/db/seed/helpers/users.ts
+++ server/src/db/seed/helpers/users.ts
@@ -1,12 +1,16 @@
+import { AuthService } from "../../../services/auth.service.js";
 import type { Transaction } from "../../index.js";
 import { users } from "../../schema/index.js";
 import { MOCK_USERS } from "../data/users.js";
 
 export const seedUsers = async (tx: Transaction, userRoleId: string) => {
-  const userRecords = MOCK_USERS.map((u) => ({
-    ...u,
-    roleId: userRoleId,
-  }));
+  const userRecords = await Promise.all(
+    MOCK_USERS.map(async (u) => ({
+      ...u,
+      passwordHash: await AuthService.hashPassword(u.password),
+      roleId: userRoleId,
+    })),
+  );
 
   return tx.insert(users).values(userRecords).returning();
 };
