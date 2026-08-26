c78da59cbc6cbaffb51db13a95330fc99a1ba9b7
Nicole Alcala <nicolealcalq@gmail.com>
2026-08-26 13:04:24 +0800

    Updated packages and configurations

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -20,4 +20,10 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  {
+    files: ['**/*.d.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+    }
+  }
 ])
