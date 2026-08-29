6ae0ba213635d8134e9aacd619c98856d7188994
Nicole Alcala <nicolealcalq@gmail.com>
2026-08-29 18:20:35 +0800

    Updated eslint rules

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -19,10 +19,15 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "no-unused-vars": "off",
+      '@typescript-eslint/no-unused-vars': 'off',
+    }
   },
   {
     files: ['**/*.d.ts'],
     rules: {
+      "no-unused-vars": "off",
       '@typescript-eslint/no-unused-vars': 'off',
     }
   }
