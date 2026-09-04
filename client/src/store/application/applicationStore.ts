b0c6119f385b93104e3027237530af62aeb62219
Nicole Alcala <nicolealcalq@gmail.com>
2026-09-04 21:02:40 +0800

    Added workArrangment in sortable columns

--- client/src/store/application/applicationStore.ts
+++ client/src/store/application/applicationStore.ts
@@ -3,6 +3,7 @@ import type {
   ApplicationStatusData,
   WorkArrangementData,
 } from "@/lib/schema/applicationSchema.ts";
+import { updateUrl } from "@/utils/url";
 import { create } from "zustand";
 import { combine } from "zustand/middleware";
 
@@ -12,7 +13,8 @@ export type SortColumn =
   | "company"
   | "location"
   | "source"
-  | "status";
+  | "status"
+  | "workArrangment";
 type SortOrder = "asc" | "desc";
 type PaginationData = {
   page?: number;
@@ -41,10 +43,10 @@ export type ApplicationTableState = {
   pagination: PaginationData;
 };
 
-const DEFAULT_SORT_BY = "appliedAt";
-const DEFAULT_SORT_ORDER: SortOrder = "desc";
-const DEFAULT_PAGE = 1;
-const DEFAULT_PAGE_SIZE = 10;
+export const DEFAULT_SORT_BY = "appliedAt";
+export const DEFAULT_SORT_ORDER: SortOrder = "desc";
+export const DEFAULT_PAGE = 1;
+export const DEFAULT_PAGE_SIZE = 10;
 
 const getInitialTableParams = (): ApplicationTableState => {
   if (typeof window === "undefined")
@@ -71,32 +73,78 @@ const getInitialTableParams = (): ApplicationTableState => {
 };
 export const useApplicationTable = create(
   combine(getInitialTableParams(), (set) => ({
-    setSortBy: (column: SortColumn) => set(() => ({ sortBy: column })),
-    setSortOrder: (order: SortOrder) => set(() => ({ sortOrder: order })),
-    setSearchTerm: (term: string) => set(() => ({ searchTerm: term })),
+    setSortBy: (column: SortColumn) => {
+      set((state) => ({
+        sortBy: column,
+        pagination: {
+          ...state.pagination,
+          page: DEFAULT_PAGE,
+        },
+      }));
+
+      updateUrl({
+        sortBy: column,
+        page: DEFAULT_PAGE,
+      });
+    },
+    setSortOrder: (order: SortOrder) =>
+      set((state) => ({
+        sortOrder: order,
+        pagination: {
+          ...state.pagination,
+          page: DEFAULT_PAGE,
+        },
+      })),
+    setSearchTerm: (term: string) =>
+      set((state) => ({
+        searchTerm: term || undefined,
+        pagination: {
+          ...state.pagination,
+          page: DEFAULT_PAGE,
+        },
+      })),
     setPagination: (pagination: Partial<PaginationData>) =>
-      set((state) => ({ pagination: { ...state.pagination, ...pagination } })),
+      set((state) => ({
+        pagination: {
+          ...state.pagination,
+          ...pagination,
+        },
+      })),
     setSelectedApplications: (applications: ApplicationTableData[]) =>
       set({
         selectedApplications: applications,
       }),
     setFilters: ({ filter, type }: FiltersArg) =>
       set((state) => {
-        if (type === "workArrangement")
+        if (type === "workArrangement") {
           return {
             workArrangementFilters: [
               ...(state.workArrangementFilters ?? []),
               filter,
             ],
+            pagination: {
+              ...state.pagination,
+              page: DEFAULT_PAGE,
+            },
           };
+        }
 
-        return { statusFilters: [...(state.statusFilters ?? []), filter] };
+        return {
+          statusFilters: [...(state.statusFilters ?? []), filter],
+          pagination: {
+            ...state.pagination,
+            page: DEFAULT_PAGE,
+          },
+        };
       }),
     resetFilters: () =>
       set((state) => ({
-        ...state,
         workArrangementFilters: undefined,
         statusFilters: undefined,
+        pagination: {
+          ...state.pagination,
+          page: DEFAULT_PAGE,
+        },
       })),
   })),
 );
