96999e614f174b5c18d07b1db29ccb48d2e3a2b2
Nicole Alcala <nicolealcalq@gmail.com>
2026-09-20 17:43:01 +0800

    Removed z-index

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -20,7 +20,6 @@ interface AppBarProps extends MuiAppBarProps {
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
 })<AppBarProps>(({ theme }) => ({
-  zIndex: theme.zIndex.drawer + 1,
   transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
